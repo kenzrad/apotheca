8f958f263a4f024b9e6b4e31bee95617aa882fe6
Slowpossum <tsmithva@verizon.net>
2019-06-29 13:54:11 -0400

    Initial commit.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>This is a page, wow!</h1>
+    </>
+  );
+}
+
+export default App;
