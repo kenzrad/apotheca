7f9a2c6fc587f798c771a7bc76c03709a6d94f65
pcaserta <patrick.caserta@live.longwood.edu>
2019-07-06 13:58:45 -0400

    merge attempt

--- models/element.js
+++ models/element.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const productSchema = new Schema({
+    name: String,
+    src: String,
+    price: String,
+    tags: String
+})
 
 const elementSchema = new Schema({
     name: { type: String, required: true },
