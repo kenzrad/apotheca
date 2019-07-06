62ea1f878da7dfd1e6d3642c69e6b47603faec0c
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-07-06 13:48:28 -0400

    navbar wrapper etc etc etc

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
+import Item from "../../components/Item";
+import Navbar from "../../components/Navbar";
+import Wrapper from "../../components/Wrapper";
+import MainArticle from "../../components/MainArticle";
+import SideArticle from "../../components/SideArticle";
 
 class Main extends Component {
   render() {
     return (
       <>
-        <Nav />
           <Wrapper>
             <MainArticle class="mainArticle">
               {/* photograph here */}
@@ -18,7 +22,6 @@ class Main extends Component {
               {/* photograph/article e */}
             </SideArticle>
           </Wrapper>
-        <Footer />
       </>
     );
   }
