8049c073181b5cb5a66b3e22c76ad4084507ac78
unknown <warnerak@dukes.jmu.edu>
2019-07-08 20:58:51 -0400

    first attempt to push to heroku app

---
+++ controllers/componentController.js
@@ -0,0 +1,40 @@
+const db = require("../models");
+//Defining methods for the componentController
+
+
+module.exports = {
+    findAll: function (req, res) {
+        db.Component
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => {
+                res.json(dbModel);
+            })
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function (req, res) {
+        db.Component
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function (req, res) {
+        db.Component
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.Component
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function (req, res) {
+        db.Component
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};
