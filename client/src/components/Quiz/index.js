20bbf366cb56ebcf36841e2aae14af7cb2cc5106
Slowpossum <tsmithva@verizon.net>
2019-07-11 19:17:27 -0400

    Quiz component in place

---
+++ client/src/components/Quiz/index.js
@@ -0,0 +1,158 @@
+import React, { Component } from "react";
+import "./style.css";
+import comparisonArr from './comparisonArr.json';
+
+let choices = [];
+
+class Quiz extends Component {
+    optionClickHandler = e => {
+        let val = e.target.getAttribute("data-answer");
+        let currQuestion = e.currentTarget.getAttribute("data-question") - 1;
+
+        if (e.target.classList.contains("options") === false) {
+            if (!e.target.classList.contains("chosen")) {
+                choices[currQuestion] = val;
+                e.target.classList.add("chosen");
+
+                for (let child of e.currentTarget.childNodes) {
+                    if (!child.classList.contains("chosen") && child.style.display !== "none") {
+                        child.style.height = `${child.offsetHeight - 16}px`;
+                        child.classList.add("hiddenOption");
+                        setTimeout(() => {
+                            child.style.display = "none";
+                        }, 320);
+                    }
+                }
+            } else {
+                choices[currQuestion] = "empty";
+
+                for (let child of e.currentTarget.childNodes) {
+                    if (child.classList.contains("hiddenOption")) {
+                        child.classList.remove("hiddenOption");
+                        child.classList.add("showOption");
+                        child.style.display = "";
+                        setTimeout(() => {
+                            child.classList.remove("showOption");
+                            child.removeAttribute("style");
+                        }, 320);
+                    } else {
+                        child.classList.remove("chosen");
+                    }
+                }
+            }
+
+        }
+    }
+
+    submitQuiz = () => {
+        let incomplete = false;
+
+        for (let i = 0; i <= 9; i++) {
+            if (choices[i] === undefined) {
+                incomplete = true;
+                break;
+            }
+        }
+        
+        if (incomplete === false) {
+            for (let n = 0; n <= 9; n++) {
+                choices[n] = comparisonArr[n + 1][choices[n]];
+            }
+
+            console.log(choices);
+        } else {
+            //Show modal asking to finish quiz or something.
+            console.log("Please answer all questions");
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <h3>What is your age range?</h3>
+                <div className="options" data-question="1" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Under 20</p>
+                    <p data-answer="B">20-30</p>
+                    <p data-answer="C">30-50</p>
+                    <p data-answer="D">Over 50</p>
+                </div>
+
+                <h3>When you wake up how is your skin?</h3>
+                <div className="options" data-question="2" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Irritated, red, and blotchy.</p>
+                    <p data-answer="B">Greasy, oil build up.</p>
+                    <p data-answer="C">Dry and flakey.</p>
+                    <p data-answer="D">No issues, ready to go.</p>
+                </div>
+
+                <h3>Day-to-day your skin:</h3>
+                <div className="options" data-question="3" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Has visible pores along t-zone and oil build up throughout the day.</p>
+                    <p data-answer="B">Is tight and irritated, has dry patches along the cheeks and chin.</p>
+                    <p data-answer="C">Rarely gets and breakouts, has little pores long t-zone, and rarely any oil buildup.</p>
+                    <p data-answer="D">Frequently has breakout and has irritated skin.</p>
+                </div>
+
+                <h3>Top skin concern:</h3>
+                <div className="options" data-question="4" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Hydration</p>
+                    <p data-answer="B">Anti-Aging</p>
+                    <p data-answer="C">Acne Control</p>
+                    <p data-answer="D">Oil Control</p>
+                </div>
+
+                <h3>Skin tone:</h3>
+                <div className="options" data-question="5" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Fair</p>
+                    <p data-answer="B">Medium-Fair</p>
+                    <p data-answer="C">Medium-Dark</p>
+                    <p data-answer="D">Dark</p>
+                </div>
+
+                <h3>How sensitive is your skin?</h3>
+                <div className="options" data-question="6" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Sensitive skin conditions (i.e.: eczema)</p>
+                    <p data-answer="B">Breaks out easy and prone to irritation</p>
+                    <p data-answer="C">Certain things make my skin react, but I'm usually okay.</p>
+                    <p data-answer="D">Not sensitive.</p>
+                </div>
+
+                <h3>Current living environment:</h3>
+                <div className="options" data-question="7" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Rural Location</p>
+                    <p data-answer="B">City Life</p>
+                    <p data-answer="C">Humid</p>
+                    <p data-answer="D">Dry</p>
+                </div>
+
+                <h3>How often do you wear sunscreen?</h3>
+                <div className="options" data-question="8" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Always</p>
+                    <p data-answer="B">Often</p>
+                    <p data-answer="C">Sometimes</p>
+                    <p data-answer="D">Rarely</p>
+                </div>
+
+                <h3>Out of the following what is your top skin concern?</h3>
+                <div className="options" data-question="9" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Post-breakout scarring.</p>
+                    <p data-answer="B">Sun-damage</p>
+                    <p data-answer="C">Tight cheeks, chapped lips</p>
+                    <p data-answer="D">Large, visible pores</p>
+                </div>
+
+                <h3>Out of the following what is your top skin concern?</h3>
+                <div className="options" data-question="10" onClick={this.optionClickHandler}>
+                    <p data-answer="A">Wrinkles</p>
+                    <p data-answer="B">Dehydrated skin</p>
+                    <p data-answer="C">T-zone oil</p>
+                    <p data-answer="D">Irritated/sensitve skin</p>
+                </div>
+
+                <button onClick={this.submitQuiz}>Submit</button>
+            </>
+        );
+    }
+}
+
+export default Quiz;
