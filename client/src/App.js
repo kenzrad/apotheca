cdd96d15b84c16d7d328969c20b2bde0a1a85b94
Slowpossum <tsmithva@verizon.net>
2019-07-12 17:51:58 -0400

    Wireframe of login/signup page finished.

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
 import Main from "./pages/Main";
 import Profile from "./pages/Profile";
 import Elements from "./pages/Elements";
 import NoMatch from "./pages/NoMatch";
 import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/Main" component={Main} />
-          <Route exact path="/Profile/:id" component={Profile} />
-          <Route exact path="/Elements" component={Elements} />
-          <Route exact path="/Elements/:id" component={Elements} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  state = {
+    loginUsername: "",
+    loginPassword: "",
+    username: "",
+    password: "",
+    firstName: "",
+    vegan: false,
+    hypoallergenic: false
+  }
+
+  handleInputChange = event => {
+    let { value, name } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleCheckbox = event => {
+    let { name, checked } = event.target;
+
+    this.setState({
+      [name]: checked
+    });
+  }
+  
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route 
+              exact path="/" 
+              render={() => <Login handleCheckbox = {this.handleCheckbox} handleInputChange={this.handleInputChange} />} />
+            <Route exact path="/Main" component={Main} />
+            <Route exact path="/Profile/:id" component={Profile} />
+            <Route exact path="/Elements" component={Elements} />
+            <Route exact path="/Elements/:id" component={Elements} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
