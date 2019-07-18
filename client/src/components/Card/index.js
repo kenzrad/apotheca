15040d8c08c59cf1d8696ed8e3e583bbcf8a31f9
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-07-18 19:46:23 -0400

    elements page styled

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -17,26 +17,26 @@ export function Benefits(props) {
 export function Card(props) {
   return (
     <>
-    <div 
-      className="item" 
-      key={props.key + "-first"}
-      style={{
-        backgroundImage: 
-        // `url('/assets/images/elements/almond.png')` 
-        // ? `url('/assets/images/elements/almond.png')` 
-        `${props.image}`
-        ? `url('${props.image}')`
-        : `url('https://via.placeholder.com/400')` 
-      }}>
-      <div className="itemDetails" key={props.key + "-second"}>
-        {/* Props.description will appear when image is hovered over. */}
-        <h2 className="itemName" key={props.key + "-third"}>{props.name}</h2>
-        <h3 className="itmeCat" key={props.key + "-fourth"}>{props.category}</h3>
-        {props.benefits.map(benefit => (
-          <p key={props.key + benefit}>{benefit}</p>
-        ))}
+      <div className ="item">
+        <div 
+          className="itemImage" 
+          key={props.key + "-first"}
+          style={{
+            backgroundImage: 
+            `${props.image}`
+            ? `url('${props.image}')`
+            : `url('https://via.placeholder.com/400')` 
+          }}>
+        </div>
+        <div className="itemDetails" key={props.key + "-second"}>
+
+          <h1 className="itemName" key={props.key + "-third"}>{props.name}</h1>
+          {/* <h2 className="itemCat" key={props.key + "-fourth"}>{props.category}</h2> */}
+          {props.benefits.map(benefit => (
+            <p className="itemText" key={props.key + benefit}>{benefit}</p>
+          ))}
+        </div>
       </div>
-    </div>
     </>
   );
 }
